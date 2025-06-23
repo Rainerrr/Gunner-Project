aa7ea3134b3946a28a08ea978eebdb3f668c350d
Rainerrr <renebit257@gmail.com>
2025-06-23 20:01:08 +0300

    rotate to target from map

--- Assets/Scripts/TargetIcon.cs
+++ Assets/Scripts/TargetIcon.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetIcon : MonoBehaviour
 {
     public Target linkedTarget;
+    [SerializeField] Turret turret;
+    [SerializeField] Button button;
     // Start is called before the first frame update
     void Start()
     {
-        
+        turret = FindAnyObjectByType<Turret>();
+        this.button.onClick.AddListener(() => turret.RotateToTarget(linkedTarget));
     }
 
     // Update is called once per frame
